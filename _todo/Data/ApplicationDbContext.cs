ce0bbfb8a86d5ddae25436f7826fb8bf217028db
esim7 <makishev@yahoo.com>
2020-06-26 17:57:31 +0600

    Добавьте файлы проекта.

---
+++ _todo/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _todo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _todo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ _todo/Models/Note.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace _todo.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeadlineTime { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
