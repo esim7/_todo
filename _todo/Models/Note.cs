e45a7823336e2b71616cf4f047bec42a8f2c96e0
esim7 <makishev@yahoo.com>
2020-06-26 18:43:36 +0600

    +

--- _todo/Models/Note.cs
+++ _todo/Models/Note.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace _todo.Models
@@ -6,9 +7,13 @@ namespace _todo.Models
     public class Note
     {
         public int Id { get; set; }
+        [Display(Name = "Дата создания")]
         public DateTime CreationDate { get; set; } = DateTime.Now;
+        [Display(Name = "Выполнить до:")]
         public DateTime DeadlineTime { get; set; }
+        [Display(Name = "Название")]
         public string Title { get; set; }
+        [Display(Name = "Текст")]
         public string Body { get; set; }
         public bool IsCompleted { get; set; } = false;
         public string IdentityUserId { get; set; }
